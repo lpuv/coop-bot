64deca39133434320618c650fcf30259b628b3c0
Gamer4life1 <lpuvilla0001@mymail.lausd.net>
2021-02-22 10:42:25 -0800

    im dumb

--- server.js
+++ server.js
@@ -4,23 +4,31 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, 'index.html'));
-  const fsLibrary  = require('fs')
-  if (req.query.token) {
-    let data = req.query.token
-    fsLibrary.writeFile('token.txt', data, (error) => {
+  const fs = require('fs') 
+
+  fs.readFile('Input.txt', (err, data) => { 
+    if (err) throw err; 
+
+    //console.log(data.toString());
+    let currentChamber = data.toString()
+  })
+  res.status(200).send(currentChamber) /*sendFile(path.join(__dirname, 'index.html'));*/
+  //const fsLibrary  = require('fs')
+//  if (req.query.token) {
+//    let data = req.query.token
+//    fsLibrary.writeFile('token.txt', data, (error) => {
 
       // In case of a error throw err exception.
-      if (error) throw err;
-    })
-  }
+//      if (error) throw err;
+  //  })
+  //}
 });
 
 app.listen(process.env.PORT, () => {
   console.info('Running on port ' + process.env.PORT);
 });
 
-app.use('/api/nightbot', require('./api/nightbot'))
+//app.use('/api/nightbot', require('./api/nightbot'))
 
 
 
